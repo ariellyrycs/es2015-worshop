e0ec6ccf09a98b4eade1d71b8ad58e9bddeb0106
Ariel Robles <arobles@switchfly.com>
2016-02-23 15:05:55 +0000

    Add some slides and modify packge.json

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,12 @@ let gulp = require('gulp'),
   fs = require('fs'),
   path = require('path');
 
-const gulpDir = path.join(__dirname, 'tasks'),
+const GULP_DIR = path.join(__dirname, 'tasks'),
   //load all files in task dir
   jsFiles = new RegExp('^.*\.(js)$', 'i');
 
-fs.readdirSync(gulpDir).forEach(fileName => {
-  if(jsFiles.test(fileName)) require(path.join(gulpDir, fileName));
+fs.readdirSync(GULP_DIR).forEach(fileName => {
+  if(jsFiles.test(fileName)) require(path.join(GULP_DIR, fileName));
 });
 
 gulp.task('js', ['lint', 'build:js']);
